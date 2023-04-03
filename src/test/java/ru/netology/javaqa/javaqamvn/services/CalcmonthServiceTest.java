7149f0b4b23d010326f2e5e0c6cfd81391e2ac42
Murat Oralbaev <successmanonly@gmail.com>
2023-04-03 22:29:17 +0300

    CalcmonthTest.java changed

--- src/test/java/ru/netology/javaqa/javaqamvn/services/CalcmonthServiceTest.java
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/CalcmonthServiceTest.java
@@ -1,19 +1,26 @@
 package ru.netology.javaqa.javaqamvn.services;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CalcmonthServiceTest {
-    @Test
-    public void couldRest() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/money.csv")
+    public void couldRest(int expected,int income,int expence, int treshold) {
         CalcmonthService service = new CalcmonthService();
-        int expected = 3;
-        int actual = service.calculate(10000,3000,20000);
-        Assertions.assertEquals(expected,actual);
+        //int expected = 3;
+        int actual = service.calculate(income, expence, treshold);
+        Assertions.assertEquals(expected, actual);
     }
-    @Test
-    public void couldRestLess() {
-        CalcmonthService service = new CalcmonthService();
-        int expected = 2;
-        int actual = service.calculate(100000, 60000, 150000);
-    }
+
+    //@Test
+    //public void couldRestLess() {
+       // CalcmonthService service = new CalcmonthService();
+        //int expected = 2;
+        //int actual = service.calculate(100000, 60000, 150000);
+      //  Assertions.assertEquals(expected, actual);
+   // }
 }
